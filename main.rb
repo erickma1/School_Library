0453362e9f19474c66602cb21639ca5ffc54bd10
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-11-14 19:41:26 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -27,3 +27,18 @@ def choose_number(choice, app)
     app.list_all_rentals
   end
 end
+
+def main
+  app = App.new
+  loop do
+    render_choices
+    choice = gets.chomp.to_i
+    if choice >= 7
+      puts 'Thank you for using this app'
+      break
+    end
+    choose_number(choice, app)
+  end
+end
+
+main
