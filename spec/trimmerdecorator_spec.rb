781cde4ea8002561b0ca6ce83f760e9417e090b6
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-11-15 20:27:34 +0000

    Update trimmerdecorator_spec.rb

--- spec/trimmerdecorator_spec.rb
+++ spec/trimmerdecorator_spec.rb
@@ -1,11 +1,26 @@
+# trimmerdecorator_spec.rb
+
 require_relative '../trimmerdecorator'
+require_relative '../nameable'
 
 describe TrimmerDecorator do
-  before :each do
-    @trimmer_decorator = TrimmerDecorator.new('Ichsan')
+  let(:nameable) { instance_double('Nameable', correct_name: 'Jonathanuous') }
+  let(:trimmer_decorator) { TrimmerDecorator.new(nameable) }
+
+  describe '#new' do
+    it 'should be an instance of TrimmerDecorator' do
+      expect(trimmer_decorator).to be_an_instance_of(TrimmerDecorator)
+    end
+
+    it 'should inherit from BaseDecorator' do
+      expect(trimmer_decorator).to be_a(BaseDecorator)
+    end
   end
 
-  it 'should return an instance of TrimmerDecorator' do
-    expect(@trimmer_decorator).to be_an_instance_of TrimmerDecorator
+  describe '#correct_name' do
+    it 'should return the correctly trimmed name' do
+      allow(nameable).to receive(:correct_name).and_return('Jonathanuous')
+      expect(trimmer_decorator.correct_name).to eql('Jonathanuo')
+    end
   end
 end
